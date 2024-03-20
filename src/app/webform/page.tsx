f8fac3557e2daeeec9f7ad2d0429ba54811d2c3e
tamana007 <tamana.efatwira1@gmail.com>
2024-03-20 12:16:06 -0700

    Data collected

--- src/app/webform/page.tsx
+++ src/app/webform/page.tsx
@@ -12,70 +12,76 @@ const WebForm: React.FC = () => {
   interface OptionType {
     _id: number;
     username: string;
-  };
-
+  }
 
   const [options, setOptions] = useState<OptionType[]>([]);
-  const [userName,setUserName]=useState<string>('');
-  const [sellerName, setSellerName]=useState<string>('');
-  const [phoneNumber,setPhoneNumber]=useState<string>('');
-  const [otherPhone,setOtherPhone]=useState<string>('');
-  const [sellerEmail,setSellerEmail]=useState<string>('');
-  const [propertyAddress,setPropertyAddress]=useState<string>('');
-  const [notes,setNotes]=useState<string>('');
-  const [motivation,setMotivation]=useState<string>('');
-  const [idealPrice,setIdealPrice]=useState<number|undefined>(undefined);
+  const [userName, setUserName] = useState<string>("");
+  const [sellerName, setSellerName] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [otherPhone, setOtherPhone] = useState<string>("");
+  const [sellerEmail, setSellerEmail] = useState<string>("");
+  const [propertyAddress, setPropertyAddress] = useState<string>("");
+  const [notes, setNotes] = useState<string>("");
+  const [motivation, setMotivation] = useState<string>("");
+  const [idealPrice, setIdealPrice] = useState<number | undefined>(undefined);
 
   const handleNameChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedValue = event.target.value;
     setUserName(selectedValue);
     // console.log('optionsss',userName);
-    
   };
-  const handleSellerNameChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
-    const change=e.target.value;
+  const handleSellerNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const change = e.target.value;
     setSellerName(change);
     // console.log('selerrrr',sellerName);
-  }
-  const handlePhoneNumber=(e:React.ChangeEvent<HTMLInputElement>)=>{
+  };
+  const handlePhoneNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPhoneNumber(e.target.value);
-console.log('phone',phoneNumber);
-  }
-  const handleOtherPhone=(e:React.ChangeEvent<HTMLInputElement>)=>{
+    console.log("phone", phoneNumber);
+  };
+  const handleOtherPhone = (e: React.ChangeEvent<HTMLInputElement>) => {
     setOtherPhone(e.target.value);
     // console.log('other phone',otherPhone);
-    
-  }
-  const handleOtherEmail=(e:React.ChangeEvent<HTMLInputElement>)=>{
-setSellerEmail(e.target.value);
-console.log('email',sellerEmail);
+  };
+  const handleOtherEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSellerEmail(e.target.value);
+    console.log("email", sellerEmail);
+  };
 
-  }
-
-  const handlePropertyAddress=(e:React.ChangeEvent <HTMLInputElement>)=>{
+  const handlePropertyAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPropertyAddress(e.target.value);
-    console.log('property address',propertyAddress);
-    
-
-  }
-  const handleNotes=(e:React.ChangeEvent <HTMLInputElement>)=>{
+    console.log("property address", propertyAddress);
+  };
+  const handleNotes = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setNotes(e.target.value);
-    console.log('notes',notes);
-    
-  }
-  const handleMotivation=(e:React.ChangeEvent <HTMLInputElement>)=>{
+    console.log("notes", notes);
+  };
+  const handleMotivation = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setMotivation(e.target.value);
-    console.log('motivation',motivation);
+    console.log("motivation", motivation);
+  };
+  const handleIdealPrice = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const price = Number(e.target.value);
+    setIdealPrice(price);
+    console.log("ideal price", idealPrice);
+  };
+  const data = {
+    idealPrice,
+    motivation,
+    notes,
+    propertyAddress,
+    sellerEmail,
+    otherPhone,
+    phoneNumber,
+    sellerName,
+    userName,
+  };
+  const handleSubmit=()=>{
+    console.log('submitted');
+    console.log('data----------',data);
     
-
-  }
-  const handleIdealPrice=(e:React.ChangeEvent <HTMLInputElement>)=>{
-    setIdealPrice(e.target.value)
-    console.log('ideal price',idealPrice);
     
-
   }
-
 
   useEffect(() => {
     const getOptions = async () => {
@@ -83,11 +89,9 @@ console.log('email',sellerEmail);
         const getApi = await fetch("/api/register");
 
         const result = await getApi.json();
-
-        // console.log(result.options, "reeeeeeeeeeeeeeeeeeeeeeeeeee");
         setOptions(result.options);
       } catch (error) {
-        console.log("error fetching users",error);
+        console.log("error fetching users", error);
       }
     };
     getOptions();
@@ -95,10 +99,11 @@ console.log('email',sellerEmail);
   }, []);
 
   return (
-    <div className="flex justify-center items-center h-screen text-black">
-      <div className="w-full max-w-xl p-6 bg-gray-100 rounded-lg">
+    // <div className="flex justify-center items-center h-screen text-black">
+    <div className="flex justify-center items-center min-h-screen text-black">
+      <div className="w-full max-w-4xl p-6 bg-gray-100 rounded-lg">
         <h1 className="text-3xl font-bold mb-6 text-center text-black">
-          Texas Propmover LLC  LEADS
+          Texas Propmover LLC LEADS
         </h1>
 
         {/* Dropdown */}
@@ -107,7 +112,7 @@ console.log('email',sellerEmail);
         </div>
         <div className="mb-4 text-black">
           <select
-          onChange={handleNameChange}
+            onChange={handleNameChange}
             className="w-full px-3 py-2 border rounded-md"
             style={{ color: "black" }}
           >
@@ -115,7 +120,11 @@ console.log('email',sellerEmail);
               Select your Name
             </option>
             {options.map((option, index) => (
-              <option key={index} value={option.username} className="text-black">
+              <option
+                key={index}
+                value={option.username}
+                className="text-black"
+              >
                 {option.username}
               </option>
             ))}
@@ -151,47 +160,52 @@ console.log('email',sellerEmail);
             Seller's Phone
           </label>
           <div className="w-full border-solid hover:border-dotted">
-            <input  type="text"
-            id="sellerPhone"
-          onChange={handlePhoneNumber}
-            className="w-full px-1 py-1 border rounded-md"
-            placeholder="Enter seller's phone number"></input>
+            <input
+              type="text"
+              id="sellerPhone"
+              onChange={handlePhoneNumber}
+              className="w-full px-1 py-1 border rounded-md"
+              placeholder="Enter seller's phone number"
+            ></input>
           </div>
         </div>
-       
-        
+
         <div className="mb-4">
           <label htmlFor="sellerPhone" className="block mb-1 text-black">
             Seller's Email
           </label>
           <div className="w-full border-solid hover:border-dotted">
-            <input  type="text"
-            onChange={handleOtherPhone}
-            id="sellerPhone"
-            className="w-full px-1 py-1 border rounded-md"
-            placeholder="Enter seller's phone number"></input>
-          </div>
-
-          <div className="w-full border-solid hover:border-dotted">
-            <select>
-              <option>Other</option>
-              <option>two</option>
-            </select>
-            <input  type="text"
-            id="sellerPhone"
-            onChange={handleOtherEmail}
-            className="w-5/6 px-1 py-1 border rounded-md"
-            placeholder="Enter seller's phone number"></input>
+            <input
+              type="text"
+              onChange={handleOtherPhone}
+              id="sellerPhone"
+              className="w-full px-1 py-1 border rounded-md"
+              placeholder="Enter seller's phone number"
+            ></input>
           </div>
         </div>
 
+        <div className="mb-4">
+          <label htmlFor="sellerPhone" className="block mb-1 text-black">
+            Other Email
+          </label>
+          <div className="w-full border-solid hover:border-dotted">
+            <input
+              type="text"
+              onChange={handleOtherEmail}
+              id="sellerPhone"
+              className="w-full px-1 py-1 border rounded-md"
+              placeholder="Enter seller's phone number"
+            ></input>
+          </div>
+        </div>
         {/* Input for property address */}
         <div className="mb-4">
           <label htmlFor="propertyAddress" className="block mb-1 text-black">
             Property Address
           </label>
           <input
-          onChange={handlePropertyAddress}
+            onChange={handlePropertyAddress}
             type="text"
             id="propertyAddress"
             className="w-full px-3 py-2 border rounded-md"
@@ -205,7 +219,7 @@ console.log('email',sellerEmail);
             Note
           </label>
           <textarea
-          onChange={handleNotes}
+            onChange={handleNotes}
             id="note"
             className="w-full px-3 py-2 border rounded-md"
             rows={4}
@@ -213,25 +227,19 @@ console.log('email',sellerEmail);
           ></textarea>
         </div>
 
-  
         <div className=" mb-4">
           <div>
             <label htmlFor="motivation" className="block mb-1 text-black">
               Motivation
             </label>
-            <select className="w-full px-3 py-2 border rounded-md" 
-            onChange={handleMotivation}>
-            <option>
-             Please select
-              </option>
-              <option>
-                Motivated
-              </option>
-              <option>
-                Not Motivated
-              </option>
+            <select
+              className="w-full px-3 py-2 border rounded-md"
+              onChange={handleMotivation}
+            >
+              <option>Please select</option>
+              <option>Motivated</option>
+              <option>Not Motivated</option>
             </select>
-           
           </div>
           <div>
             <label htmlFor="idealPrice" className="block mb-1 text-black">
@@ -249,7 +257,9 @@ console.log('email',sellerEmail);
 
         {/* Submit button */}
         <div className="text-center">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors">
+          <button 
+          onClick={handleSubmit}
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors">
             Submit
           </button>
         </div>
